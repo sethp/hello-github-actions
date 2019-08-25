e50e0ba052346c5beaeb5640b6208caf5eb49759
Seth Pellegrino <seth@codecopse.net>
2019-08-25 10:21:06 -0700

    more syntax

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $MY_NAME \(sshhh don't tell anyone about $SECRET\)"
+sh -c "echo Hello world my name is $MY_NAME \(sshhh don\'t tell anyone about $SECRET\)"
 
